38cbc93deea22185906cf1a804dba4b7b3ee40d7
Nina-pinheiro <ninapinheiro@usp.br>
2020-10-05 21:40:16 -0300

    Exercício 2 da prova

---
+++ bash_script.sh
@@ -0,0 +1,8 @@
+# Um comando que mostre pasta/ diretório onde você está
+pwd 
+# Um comando que gere uma lista com detalhes do conteúdo da sua pasta HOME e redirecione essa lista para o arquivo: lista_HOME.txt
+# Forçar que vai para HOME 
+cd ~  
+ls -l  > lista_HOME.txt
+# um comando que imprima a data atual na tela 
+date
