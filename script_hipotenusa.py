365c83760732b75483f247f96fac6765283c1bef
Nina-pinheiro <ninapinheiro@usp.br>
2020-10-05 21:42:09 -0300

    Exercício 3 da prova

---
+++ script_hipotenusa.py
@@ -0,0 +1,34 @@
+# Importando as Bibliotecas necessárias
+import sys
+import math 
+
+# O usuário colocam números inteiros na linha de comando
+x = (sys.argv[1])
+y = (sys.argv[2])
+
+# Verificando se os valores são inteiros 
+try:
+    x = int(x)
+    y = int(y)
+except ValueError:
+    print("Por favor, informe apenas números inteiros")
+    sys.exit(1)
+
+
+# Colocar condição que os números tem que ser positivos
+if x > 0  and y > 0:
+# Colocar a condição do exercício que os números "X " e "Y" devem ser menor que 1000
+    if x < 1000 and y < 1000:
+        print("Está correto")
+# Realizar a equação - teorema de pitagoras
+        z = math.sqrt(x**2 + y**2)
+        # Printar o valor de z
+        print(" O valor de z é " , z)
+    else:
+        print("Por favor, digite números menores que 1000") 
+else:
+    print("As condições de números inteiros maiores que 0 e menores que 1000 não foram atendidas")
+   
+
+
+
