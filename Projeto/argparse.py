e5c00b07cb235a41f54c45f2bc5567ef8ea0c1b7
Nina-pinheiro <ninapinheiro@usp.br>
2020-12-03 22:32:10 -0300

    Script de Argparse

---
+++ Projeto/argparse.py
@@ -0,0 +1,27 @@
+# Importando as Bibliotecas necessárias
+
+from Bio import SeqIO
+from Bio. Seq import Seq
+
+sequencia = list(SeqIO.parse("covid.txt", 'fasta'))
+
+
+# Para 3 nucleotídeos
+
+# Sequências são declaradas como objeto por isso precisa do método "Seq". Não dá para ser usado como uma string sequencia = "ATGGGC"
+
+
+cont = {}
+
+# Loop para mostrar quais nucleotideos serão contados
+for i in ['A', 'T', 'C', 'G']:
+  for j in ['A', 'T', 'C', 'G']:
+    for a in  ['A', 'T', 'C', 'G']:
+      cont[i+j+a] = 0
+
+# Loop para a leitura dos trinucleotídeos
+sequencia=sequencia[0]
+for k in range(len(sequencia)-2):
+  cont[sequencia[k]+sequencia[k+1]+sequencia[k+2]] += 1
+
+print(cont)
